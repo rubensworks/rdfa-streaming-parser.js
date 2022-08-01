dd6ef1df86a68543966db84b29083d1d68a5cf62
Tpt <thomaspt@hotmail.fr>
2022-08-01 16:38:41 +0200

    Upgrades dependencies

--- spec/parser.js
+++ spec/parser.js
@@ -1,9 +1,8 @@
 const { RdfaParser } = require("..");
-const { ErrorSkipped } = require('rdf-test-suite');
 
 module.exports = {
   parse: function (data, baseIRI, options) {
-    return require('arrayify-stream')(require('streamify-string')(data)
+    return require('arrayify-stream').default(require('streamify-string')(data)
       .pipe(new RdfaParser(Object.assign({ baseIRI }, options))));
   },
 };
